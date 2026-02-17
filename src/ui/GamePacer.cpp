0d5107fc18bb3765f69ac86bb0d823ee01eacd22
Kilian <kischoenhaar@gmail.com>
2026-02-17 11:41:23 +0100

    feat(ui): add configurable GamePacer component

---
+++ src/ui/GamePacer.cpp
@@ -0,0 +1,64 @@
+#include "ui/GamePacer.hpp"
+
+#include <chrono>
+#include <thread>
+
+namespace ui {
+
+namespace {
+void sleepMs(const int milliseconds) {
+    if (milliseconds <= 0) {
+        return;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+}
+} // namespace
+
+void GamePacer::setMode(const PaceMode mode) {
+    mode_ = mode;
+}
+
+PaceMode GamePacer::mode() const {
+    return mode_;
+}
+
+void GamePacer::pauseShort() const {
+    switch (mode_) {
+        case PaceMode::Instant:
+            return;
+        case PaceMode::Smooth:
+            sleepMs(200);
+            return;
+        case PaceMode::Cinematic:
+            sleepMs(450);
+            return;
+    }
+}
+
+void GamePacer::pauseMedium() const {
+    switch (mode_) {
+        case PaceMode::Instant:
+            return;
+        case PaceMode::Smooth:
+            sleepMs(650);
+            return;
+        case PaceMode::Cinematic:
+            sleepMs(1300);
+            return;
+    }
+}
+
+void GamePacer::pauseLong() const {
+    switch (mode_) {
+        case PaceMode::Instant:
+            return;
+        case PaceMode::Smooth:
+            sleepMs(1200);
+            return;
+        case PaceMode::Cinematic:
+            sleepMs(2200);
+            return;
+    }
+}
+
+} // namespace ui
